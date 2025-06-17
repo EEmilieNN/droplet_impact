06c5b02a6dfe30b4164f6765c3122350ccf7f1e9
Emilien Gouffault <emilien@MacBook-Pro.local>
2025-06-17 11:36:24 +0200

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='droplets_impact_speeds',
     version='0.1.0',
-    packages=find_packages(),
-    install_requires=[],  # Add your dependencies here
+    packages=find_packages(where="model_python"),
+    install_requires=[], 
+    package_dir={'': 'model_python'},
     author='Emilien Gouffault',
     author_email='emilien.gouffault@gmail.com',
     description='This package was created for a DTU Wind&Energy Systems project to calculate the impact speeds of droplets on wind turbine blades.',
