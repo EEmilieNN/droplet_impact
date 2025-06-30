426f688dba4c1da7c7fc4afe800f3223527211e8
Emilien Gouffault <emilien.gouffault@gmail.com>
2025-06-30 13:06:06 +0200

    exception for interpolator

--- droplet_impact/utils.py
+++ droplet_impact/utils.py
@@ -126,7 +126,10 @@ def get_impact_speed(V_blade, R, Rc, n):
     if _interpolator is None:
         _interpolator = load_interpolator()
     point = np.array([[V_blade, R, Rc, n]])
-    return _interpolator(point)[0]
+    try:
+        return _interpolator(point)[0]
+    except ValueError as e:
+        return 0.0
 
 ### NREL and IEA points for n and Rc
 points_n_nrel = [
